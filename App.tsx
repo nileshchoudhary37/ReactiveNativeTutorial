4e4286fd00d5585d5264e77920fe2871238a568f
nilesh <nkc10800@gmail.com>
2026-01-19 02:55:07 +0530

    learned Image, Button, Alert, Pressable, TouchableOpacity, ScrollView

--- App.tsx
+++ App.tsx
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  Button,
+  Alert,
+  Pressable,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const onButtonPress = () => Alert.alert("Button Pressed!");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ backgroundColor:"red"}}
+      >
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <Image
+          source={require("./assets/splash-icon.png")}
+          style={styles.image1}
+        ></Image>
+        <TouchableOpacity>
+          <Image
+            source={require("./assets/splash-icon.png")}
+            style={styles.image1}
+          ></Image>
+        </TouchableOpacity>
+        <Pressable onPress={onButtonPress}>
+          <Image
+            source={{ uri: "https://picsum.photos/200/300" }}
+            style={styles.image2}
+            blurRadius={4}
+          />
+        </Pressable>
+        <Button title="Press Me" onPress={onButtonPress} />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+  },
+  image1: {
+    width: 200,
+    height: 200,
+  },
+  image2: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
   },
 });
