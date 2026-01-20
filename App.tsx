97f167c811b8cd34072d4d3b26483681bc4a690f
nilesh <nkc10800@gmail.com>
2026-01-20 22:49:59 +0530

    ImageBackground and TextInput

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
-import { ImageBackground } from 'react-native';
+import { ImageBackground, TextInput, View } from "react-native";
 
 export default function App() {
- 
   return (
-    <ImageBackground 
+    <ImageBackground
       source={require("./assets/Beautiful-flowers-background-png-image-1-min.png")}
-      style={{flex: 1}}
+      style={{ flex: 1 }}
     >
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          autoFocus
+          style={{
+            height: 40,
+            width: "80%",
+            borderRadius: 4,
+            borderWidth: 1,
+            borderColor: "grey",
+          }}
 
+          placeholder="Enter your name"
+          keyboardType="default"
+          // secureTextEntry={true}
+          // multiline={true}
+        />
+      </View>
     </ImageBackground>
   );
 }
