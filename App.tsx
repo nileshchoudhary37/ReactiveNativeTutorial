5e0973be601cc0e858c83d6a46a0bb89f4c9dbdd
nilesh <nkc10800@gmail.com>
2026-01-20 20:08:22 +0530

    ImageBackground

--- App.tsx
+++ App.tsx
@@ -1,28 +1,13 @@
-import { Button, Modal, Text, View } from "react-native";
-import React, { useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import AntDesign from '@expo/vector-icons/AntDesign';
+import { ImageBackground } from 'react-native';
 
 export default function App() {
-  const [modalVisible, setModalVisible] = useState(false);
+ 
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: "gold",
-        justifyContent: "center",
-      }}
+    <ImageBackground 
+      source={require("./assets/Beautiful-flowers-background-png-image-1-min.png")}
+      style={{flex: 1}}
     >
-      <View style={{alignItems: "center" }}>
-        <Button title="Show Modal" onPress={() => setModalVisible(true)} />
-      </View>
-      <Modal visible={modalVisible} animationType="slide">
-        <Text style={{ marginTop: 50, fontSize: 50 }}>Modal is Opened</Text>
-        <View style={{alignItems: "center", justifyContent: "center", flex: 1 }}>
-        <Button title="Show Modal" onPress={() => setModalVisible(false)} />
-          <AntDesign name="close-circle" size={44} color="red" onPress={() => setModalVisible(false)} />
-      </View>
-      </Modal>
-    </SafeAreaView>
+
+    </ImageBackground>
   );
 }
